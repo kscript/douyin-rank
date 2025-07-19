a8737c887e5a8c71fe2aba0c3ca1b7110c324d78
kscript <1258@live.cn>
2025-07-19 17:27:24 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,25 +1,27 @@
 const {
   AID,
   ANCHOR_ID,
-  ROOM_ID
+  ROOM_ID,
+  MS_TOKEN = '',
+  A_BOGUS = ''
 } = Object.assign({
   AID: '6383',
   ANCHOR_ID: '1706895689395140',
   ROOM_ID: '7422072931515206451'
 }, process.env)
-
+const signature = MS_TOKEN && A_BOGUS ? `&msToken=${MS_TOKEN}&a_bogus=${A_BOGUS}` : ''
 const list = [
   {
     type: 'popularity',
-    url: `https://live.douyin.com/webcast/ranklist/popularity/?aid=${AID}&anchor_id=${ANCHOR_ID}&room_id=${ROOM_ID}`
+    url: `https://live.douyin.com/webcast/ranklist/popularity/?aid=${AID}&anchor_id=${ANCHOR_ID}&room_id=${ROOM_ID}${signature}`
   },
   {
     type: 'hour',
-    url: `https://live.douyin.com/webcast/ranklist/hour_detail/?aid=${AID}&anchor_id=${ANCHOR_ID}&room_id=${ROOM_ID}&ranklist_type=0`
+    url: `https://live.douyin.com/webcast/ranklist/hour_detail/?aid=${AID}&anchor_id=${ANCHOR_ID}&room_id=${ROOM_ID}${signature}&ranklist_type=0`
   },
   {
     type: 'hour-new',
-    url: `https://live.douyin.com/webcast/ranklist/hour_detail/?aid=${AID}&anchor_id=${ANCHOR_ID}&room_id=${ROOM_ID}&ranklist_type=6`
+    url: `https://live.douyin.com/webcast/ranklist/hour_detail/?aid=${AID}&anchor_id=${ANCHOR_ID}&room_id=${ROOM_ID}${signature}&ranklist_type=6`
   },
   {
     type: 'creator-top',
